a04c2b28e999d9153206dc410eac248c0aeb04cb
Aditya Mehra <aix.m@outlook.com>
2022-08-10 11:53:44 +0930

    try automation of release 1

--- setup.py
+++ setup.py
@@ -1,18 +1,54 @@
 #!/usr/bin/env python3
+import os
 from setuptools import setup
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_PHAL_plugin_brightness_control_rpi', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
 
 PLUGIN_ENTRY_POINT = 'ovos-PHAL-plugin-brightness-control-rpi=ovos_PHAL_plugin_brightness_control_rpi:BrightnessControlRPIPlugin'
 setup(
     name='ovos-PHAL-plugin-brightness-control-rpi',
-    version='0.0.1',
+    version=get_version(),
     description='A plugin for OpenVoiceOS hardware abstraction layer',
     url='https://github.com/OpenVoiceOS/ovos-PHAL-plugin-brightness-control',
     author='AIIX',
     author_email='aix.m@outlook.com',
     license='Apache-2.0',
     packages=['ovos_PHAL_plugin_brightness_control_rpi'],
-    install_requires=["ovos-plugin-manager>=0.0.1"],
+    install_requires=required("requirements.txt"),
     zip_safe=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
