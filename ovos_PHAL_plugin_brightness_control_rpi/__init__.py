2f2024098cdf700db408a7330839dfc62bb3e051
Aditya Mehra <aix.m@outlook.com>
2022-08-11 19:01:58 +0930

    Add nightmode support

--- ovos_PHAL_plugin_brightness_control_rpi/__init__.py
+++ ovos_PHAL_plugin_brightness_control_rpi/__init__.py
@@ -13,12 +13,13 @@
 # limitations under the License.
 #
 
-import math
-import time
+import datetime
 import subprocess
 import threading
+import time
 from os.path import exists, join
 
+import requests
 from json_database import JsonStorage
 from mycroft_bus_client import Message
 from ovos_plugin_manager.phal import PHALPlugin
@@ -34,17 +35,31 @@ class BrightnessControlRPIPlugin(PHALPlugin):
         self.ddcutil_detected_bus = None
         self.ddcutil_brightness_code = None
         self.auto_dim_enabled = False
+        self.auto_night_mode_enabled = False
         self.timer_thread = None
+        self.nightmode_thread = None
+        self.geolocate_api_thread = None
+        self.location = None
+        self.sunset_time = None
 
+        self.geolocate_api()
+        self.start_geolocate_api_timer()
         self.is_auto_dim_enabled()
+        self.is_auto_night_mode_enabled()
+
+        LOG.info(f"Location is {self.location}")
+        LOG.info(f"Sunset time is {self.sunset_time}")
+
         self.discover()
 
         self.bus.on("phal.brightness.control.get",
                     self.query_current_brightness)
         self.bus.on("phal.brightness.control.set",
                     self.set_brightness_from_bus)
-        self.bus.on("speaker.extension.display.auto.dim.changed", 
+        self.bus.on("speaker.extension.display.auto.dim.changed",
                     self.is_auto_dim_enabled)
+        self.bus.on("speaker.extension.display.auto.nightmode.changed",
+                    self.is_auto_night_mode_enabled)
         self.bus.on("gui.page_interaction",
                     self.undim_display)
         self.bus.on("gui.page_gained_focus",
@@ -60,7 +75,8 @@ class BrightnessControlRPIPlugin(PHALPlugin):
         display_config_path_local = join(xdg_config_home(), "OvosDisplay.conf")
         if exists(display_config_path_local):
             display_configuration = JsonStorage(display_config_path_local)
-            self.auto_dim_enabled = display_configuration.get("auto_dim", False)
+            self.auto_dim_enabled = display_configuration.get(
+                "auto_dim", False)
         else:
             self.auto_dim_enabled = False
 
@@ -144,7 +160,8 @@ class BrightnessControlRPIPlugin(PHALPlugin):
             subprocess.Popen(["/usr/bin/ddcutil", "setvcp", self.ddcutil_brightness_code,
                              "--bus", self.ddcutil_detected_bus, str(level)])
         elif self.device_interface == "DSI":
-            subprocess.call(f"echo {level} > /sys/class/backlight/rpi_backlight/brightness", shell=True)
+            subprocess.call(
+                f"echo {level} > /sys/class/backlight/rpi_backlight/brightness", shell=True)
 
         LOG.info("Brightness level set to {}".format(level))
 
@@ -171,14 +188,14 @@ class BrightnessControlRPIPlugin(PHALPlugin):
                 apply_level = 255
             else:
                 apply_level = round(percent_level / 10) * 10
-            
+
             self.set_brightness(apply_level)
-            
+
     def start_auto_dim(self):
         LOG.debug("Starting auto dim")
         self.timer_thread = threading.Thread(target=self.auto_dim_timer)
         self.timer_thread.start()
-    
+
     def auto_dim_timer(self):
         while self.auto_dim_enabled:
             time.sleep(60)
@@ -187,8 +204,9 @@ class BrightnessControlRPIPlugin(PHALPlugin):
                 current_brightness = 100
             if self.device_interface == "DSI":
                 current_brightness = 255
-                
-            self.bus.emit(Message("phal.brightness.control.auto.dim.update", {"brightness": 20}))
+
+            self.bus.emit(
+                Message("phal.brightness.control.auto.dim.update", {"brightness": 20}))
             self.set_brightness(20)
 
     def stop_auto_dim(self):
@@ -196,13 +214,13 @@ class BrightnessControlRPIPlugin(PHALPlugin):
         self.auto_dim_enabled = False
         if self.timer_thread:
             self.timer_thread.join()
-            
+
     def restart_auto_dim(self):
         LOG.debug("Restarting Auto Dim")
         self.stop_auto_dim()
         self.auto_dim_enabled = True
         self.start_auto_dim()
-            
+
     def undim_display(self, message=None):
         if self.auto_dim_enabled:
             LOG.debug("Undimming display on interaction")
@@ -210,7 +228,114 @@ class BrightnessControlRPIPlugin(PHALPlugin):
                 self.set_brightness(100)
             if self.device_interface == "DSI":
                 self.set_brightness(255)
-            self.bus.emit(Message("phal.brightness.control.auto.dim.update", {"brightness": "100"}))
+            self.bus.emit(
+                Message("phal.brightness.control.auto.dim.update", {"brightness": "100"}))
             self.restart_auto_dim()
         else:
             pass
+
+    ##### AUTO NIGHT MODE HANDLING #####
+
+    def get_time_of_day(self):
+        LOG.debug("Getting time of day")
+        now = datetime.datetime.now()
+        return now.time()
+
+    def check_if_sun_has_set(self):
+        LOG.debug("Checking if sun has set")
+        # check if the time now is after the sunset time
+        current_time = self.get_time_of_day()
+        sunset_time = self.sunset_time.time()
+        if self.sunset_time:
+            if current_time > sunset_time:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def get_sunset_time(self):
+        LOG.debug("Getting sunset time")
+        if self.location:
+            location_lat = self.location["coordinate"]["latitude"]
+            location_lon = self.location["coordinate"]["longitude"]
+            response = requests.get(
+                f"http://api.sunrise-sunset.org/json?lat={location_lat}&lng={location_lon}&formatted=0")
+            response_json = response.json()
+            sunset_time_str = response_json["results"]["sunset"]
+            sunset_time = datetime.datetime.strptime(
+                sunset_time_str, "%Y-%m-%dT%H:%M:%S%z")
+            self.sunset_time = sunset_time
+        else:
+            # Set the sunset time to 10:00pm if no location is set
+            self.sunset_time = datetime.time(22, 0)
+
+    def start_auto_night_mode(self):
+        LOG.debug("Starting auto night mode")
+        self.nightmode_thread = threading.Thread(
+            target=self.auto_night_mode_timer)
+        self.nightmode_thread.start()
+
+    def auto_night_mode_timer(self):
+        while self.auto_night_mode_enabled:
+            time.sleep(60)
+            LOG.debug("Checking if it is night time")
+            if self.check_if_sun_has_set():
+                LOG.debug("It is night time")
+                self.bus.emit(
+                    Message("phal.brightness.control.auto.night.mode.enabled"))
+
+    def stop_auto_night_mode(self):
+        LOG.debug("Stopping auto night mode")
+        self.auto_night_mode_enabled = False
+        if self.nightmode_thread:
+            self.nightmode_thread.join()
+
+    def is_auto_night_mode_enabled(self):
+        display_config_path_local = join(xdg_config_home(), "OvosDisplay.conf")
+        if exists(display_config_path_local):
+            display_configuration = JsonStorage(display_config_path_local)
+            self.auto_night_mode_enabled = display_configuration.get(
+                "auto_nightmode", False)
+        else:
+            self.auto_night_mode_enabled = False
+
+        if self.auto_night_mode_enabled:
+            self.start_auto_night_mode()
+        else:
+            self.stop_auto_night_mode()
+
+    def geolocate_api(self):
+        LOG.debug("Getting Geolocation")
+        try:
+            response = requests.get("http://ip-api.com/json")
+            response_json = response.json()
+            self.location = {
+                "name": response_json["city"],
+                "coordinate": {
+                    "latitude": response_json["lat"],
+                    "longitude": response_json["lon"]
+                },
+                "timezone": response_json["timezone"],
+                "country": response_json["country"]
+            }
+        except Exception as e:
+            LOG.error("Error getting Geolocation")
+            LOG.error(e)
+            self.location = None
+
+        self.get_sunset_time()
+
+    def geolocate_api_timer(self):
+        while True:
+            time.sleep(14400)
+            self.geolocate_api()
+
+    def start_geolocate_api_timer(self):
+        self.geolocate_api_thread = threading.Thread(
+            target=self.geolocate_api_timer)
+        self.geolocate_api_thread.start()
+
+    def stop_geolocate_api_timer(self):
+        if self.geolocate_api_thread:
+            self.geolocate_api_thread.join()
